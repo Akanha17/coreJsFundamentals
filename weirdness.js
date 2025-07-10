f712a5baefc2faf01817fa1fdb0473972ce7d77e
Akanha17 <aishiashirvad@gmail.com>
2025-07-10 23:45:37 +0530

    Day1 of weirdness in JS

---
+++ weirdness.js
@@ -0,0 +1,27 @@
+let arr = ['hello', 42, true, undefined, Symbol(), 10n]
+
+console.log('the array', arr);
+console.log('the array and the types that JS returns')
+console.log('')
+
+for( elements of arr ){
+    console.log(elements, '->' ,typeof elements)
+}
+console.log('')
+// now weirdness begins
+
+let weirdos = [null, [], function (){}, NaN]
+
+console.log('weridos', weirdos)
+console.log('types of these weirdos as follows')
+console.log('')
+
+for(weird of weirdos){
+    console.log(weird,'->', typeof weird)
+}
+console.log('')
+
+// some special cases and how to properly identify weirdos
+// 1. []
+
+console.log(typeof Array.isArray([]))
